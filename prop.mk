8c2ecc061b0e41189fb77b1095d201ef8ac7f0d0
sayan7848 <sayan7848@gmail.com>
2018-12-06 17:37:44 +0000

    Revert "Update Display HAl props"
    This reverts commit 8320175c1b97e86bcc0d6eca06a52c399e1682db.

--- prop.mk
+++ prop.mk
@@ -86,6 +86,7 @@ persist.debug.coresight.config=stm-events
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
 debug.enable.sglscale=1 \
+debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.hw=0 \
 dev.pm.dyn_samplingrate=1 \
@@ -96,9 +97,10 @@ persist.hwc.mdpcomp.enable=true \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
 ro.sf.lcd_density=420 \
+sdm.debug.disable_skip_validate=1 \
+sdm.debug.rotator_downscale=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
-vendor.display.disable_skip_validate=1 \
-vendor.display.rotator_downscale=1
+vendor.display.disable_skip_validate=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -160,10 +162,6 @@ persist.rild.nitz_short_ons_0="" \
 persist.rild.nitz_short_ons_1="" \
 persist.rild.nitz_short_ons_2="" \
 persist.rild.nitz_short_ons_3=""
-
-# Property to enable display default color mode
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.display.enable_default_color_mode=0
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
